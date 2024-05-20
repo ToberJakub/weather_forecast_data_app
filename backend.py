322bfc124f245c2b8bcfeaaef8da5ee431486a21
ToberJakub <jakubtober276@gmail.com>
2024-05-20 20:06:45 +0200

    Adding functionalities

---
+++ backend.py
@@ -0,0 +1,17 @@
+import requests
+
+API_key = "56906dec8208eb4f6142224e95c88e0a"
+
+
+def get_data(place, forecast_days=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_key}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo", forecast_days=3))
